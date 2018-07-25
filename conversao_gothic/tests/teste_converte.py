2fb8429d246bd8003ba1f0488a380ce9db6bf043
phborba <philipeborba@gmail.com>
2018-07-24 22:27:02 -0300

    backup commit

---
+++ conversao_gothic/tests/teste_converte.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import json, os
+from python_caller import TestFeatureProcessor
+
+inputFilePath = os.path.join(os.path.dirname(__file__), 'origem_postgis.json')
+jsonMapeamentoPath = os.path.join(os.path.dirname(__file__), '..', 'SM_EDGV_213_PRO_v5_to_Gothic_213_2018-02-27.json')
+
+with open(inputFilePath) as inputFile:
+    origem = json.loads(inputFile.read())
+
+featureProcessor = TestFeatureProcessor(jsonMapeamentoPath)
+
+destino = []
+for feat in origem:
+    destino += featureProcessor.input(feat)
+print origem
+print destino
