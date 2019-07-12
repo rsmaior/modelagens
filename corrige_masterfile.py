f9a9f4b7c1d2d0eecd90a551cda87af8b939937b
phborba <philipeborba@gmail.com>
2019-07-12 20:50:39 -0300

    começo do conserto

--- corrige_masterfile.py
+++ corrige_masterfile.py
@@ -20,7 +20,7 @@ change_dict = {
     }
 }
 
-with open(os.path.join(os.path.dirname(__file__),'master_file_300_dsgtools_v3.json'), 'r', encoding='utf-8') as f:
+with open(os.path.join(os.path.dirname(__file__),'masterfile_300_dsgtools_v3.json'), 'r', encoding='utf-8') as f:
     master_dict = json.load(f)
 
 for class_item in master_dict['classes']:
@@ -28,12 +28,13 @@ for class_item in master_dict['classes']:
         if attr_item['nome'] in change_dict:
             if "lista_mudanca" in change_dict[attr_item['nome']]:
                 for mudanca in change_dict[attr_item['nome']]["lista_mudanca"]:
-                    for k, v in mudanca:
+                    for k, v in mudanca.items():
                         attr_item[k] = v
             if "lista_remocao" in change_dict[attr_item['nome']]:
                 for remocao in change_dict[attr_item['nome']]["lista_remocao"]:
-                    pass
+                    if remocao in attr_item:
+                        attr_item.pop(remocao)
 
 
-with open(os.path.join(os.path.dirname(__file__),'masterfile_300_dsgtools_v4.json'), 'w', encoding='utf-8') as g:
+with open(os.path.join(os.path.dirname(__file__),'masterfile300_dsgtools_v4.json'), 'w', encoding='utf-8') as g:
     g.write(json.dumps(master_dict, indent=4, sort_keys=False, ensure_ascii=False))
