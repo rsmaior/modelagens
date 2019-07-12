d4d8e971997866cbfa1325dff8e3021365301273
Felipe Diniz <diniz.ime@gmail.com>
2019-07-12 16:22:54 -0300

    arquivos de mapeamento v2

--- python_caller.py
+++ python_caller.py
@@ -51,30 +51,72 @@ class FeatureProcessor(object):
             key_attr_destiny = "attr_volta"
             key_value_origin = "valor_ida"
             key_value_destiny = "valor_volta"
-            default_key = "atributos_default_ida"
-            filter_key = "filtro_ida"
+            key_default = "atributos_default_ida"
+            key_filter = "filtro_ida"
             key_class_origin = "classe_ida"
             key_class_destiny = "classe_volta"
             key_group_origin = "tupla_ida"
             key_group_destiny = "tupla_volta"
+            key_affix_origin = "afixo_geom_ida"
+            key_affix_destiny = "afixo_geom_volta"
+            key_schema_origin = "schema_volta"
+            key_schema_destiny = "schema_ida"
+            key_class_affix_origin = "afixo_ida"
+            key_class_affix_destiny = "afixo_volta"
+            key_class_schema_origin = "schema_ida"
+            key_class_schema_destiny = "schema_volta"
+            key_agregar = "agregar_geom_ida"
         else:
             key_attr_origin = "attr_volta"
             key_attr_destiny = "attr_ida"
             key_value_origin = "valor_volta"
             key_value_destiny = "valor_ida"
-            default_key = "atributos_default_volta"
-            filter_key = "filtro_volta"
+            key_default = "atributos_default_volta"
+            key_filter = "filtro_volta"
             key_class_origin = "classe_volta"
             key_class_destiny = "classe_ida"
             key_group_origin = "tupla_volta"
             key_group_destiny = "tupla_ida"
+            key_affix_origin = "afixo_geom_volta"
+            key_affix_destiny = "afixo_geom_ida"
+            key_schema_origin = "schema_volta"
+            key_schema_destiny = "schema_ida"
+            key_class_affix_origin = "afixo_volta"
+            key_class_affix_destiny = "afixo_ida"
+            key_class_schema_origin = "schema_volta"
+            key_class_schema_destiny = "schema_ida"
+            key_agregar = "agregar_geom_volta"
+
+        featDict["fme_feature_type_original"] = featDict["fme_feature_type"]
+
+        if key_schema_origin in self.mappingDict and self.mappingDict[key_schema_origin]:
+            featDict["fme_feature_type"] = featDict["fme_feature_type"][len(self.mappingDict[key_schema_origin])+1:]
+        
+        featDict["fme_feature_type_sem_schema"] = featDict["fme_feature_type"]
+
+        if key_affix_origin in self.mappingDict and 'tipo' in self.mappingDict[key_affix_origin] and self.mappingDict[key_affix_origin]['tipo'] == 'sufixo':
+            featDict["fme_feature_type"] = featDict["fme_feature_type"][:-len(self.mappingDict[key_affix_origin][featDict["$GEOM_TYPE"]])] + self.mappingDict[key_affix_origin][featDict["$GEOM_TYPE"]]
+            featDict["fme_feature_type_sem_afixo"] = featDict["fme_feature_type_original"][:-len(self.mappingDict[key_affix_origin][featDict["$GEOM_TYPE"]])] + self.mappingDict[key_affix_origin][featDict["$GEOM_TYPE"]]
+
+        if key_affix_origin in self.mappingDict and 'tipo' in self.mappingDict[key_affix_origin] and self.mappingDict[key_affix_origin]['tipo'] == 'prefixo':
+            featDict["fme_feature_type"] = self.mappingDict[key_affix_origin][featDict["$GEOM_TYPE"]] + featDict["fme_feature_type"][len(self.mappingDict[key_affix_origin][featDict["$GEOM_TYPE"]]):]
+            featDict["fme_feature_type_sem_afixo"] = self.mappingDict[key_affix_origin][featDict["$GEOM_TYPE"]] + featDict["fme_feature_type_original"][len(self.mappingDict[key_affix_origin][featDict["$GEOM_TYPE"]]):]
 
         should_map = False
         for classmap in self.mappingDict['mapeamento_classes']:
             if "sentido" not in classmap or ("sentido" in classmap and classmap["sentido"] == self.mappingType):
-                if classmap[key_class_origin] == featDict["fme_feature_type"]:
-                    if filter_key in classmap:
-                        if not self.evaluateFilter(featDict, classmap[filter_key]):
+                if key_class_affix_origin in classmap and classmap[key_class_affix_origin] and key_class_schema_origin in classmap and classmap[key_class_schema_origin]:
+                    class_name = featDict["fme_feature_type_original"]
+                elif key_class_affix_origin in classmap and classmap[key_class_affix_origin]:
+                    class_name = featDict["fme_feature_type_sem_schema"]
+                elif key_class_schema_origin in classmap and classmap[key_class_schema_origin]:
+                    class_name = featDict["fme_feature_type_sem_afixo"]
+                else:
+                     class_name = featDict["fme_feature_type"]
+                
+                if classmap[key_class_origin] == class_name:
+                    if key_filter in classmap:
+                        if not self.evaluateFilter(featDict, classmap[key_filter]):
                             continue
                     should_map = True
         if not should_map:
@@ -83,8 +125,8 @@ class FeatureProcessor(object):
 
         mappedFeat = copy.deepcopy(featDict)
 
-        if default_key in self.mappingDict:
-            for default in self.mappingDict[default_key]:
+        if key_default in self.mappingDict:
+            for default in self.mappingDict[key_default]:
                 mappedFeat[default["nome_atributo"]] = default["valor"]
 
         if 'mapeamento_atributos' in self.mappingDict:
@@ -94,38 +136,77 @@ class FeatureProcessor(object):
                     if "traducao" in attmap:
                         for valuemap in attmap["traducao"]:
                             if u"{0}".format(valuemap[key_value_origin]) == u"{0}".format(featDict[attmap[key_attr_origin]]) and ("sentido" not in valuemap or ("sentido" in valuemap and valuemap["sentido"] == self.mappingType)):
-                                mappedFeat[attmap[key_attr_destiny]] = valuemap[key_value_destiny]
+                                if "concatenar" in valuemap and attmap[key_attr_destiny] in mappedFeat and mappedFeat[attmap[key_attr_destiny]]:
+                                    mappedFeat[attmap[key_attr_destiny]] = mappedFeat[attmap[key_attr_destiny]] + ' | ' + valuemap[key_value_destiny]
+                                else:
+                                    mappedFeat[attmap[key_attr_destiny]] = valuemap[key_value_destiny]
 
         if 'mapeamento_classes' in self.mappingDict:
             for classmap in self.mappingDict['mapeamento_classes']:
                 if "sentido" not in classmap or ("sentido" in classmap and classmap["sentido"] == self.mappingType):
-                    if classmap[key_class_origin] == featDict["fme_feature_type"]:
-                        if filter_key in classmap:
-                            if not self.evaluateFilter(featDict, classmap[filter_key]):
+                    if key_class_affix_origin in classmap and classmap[key_class_affix_origin] and key_class_schema_origin in classmap and classmap[key_class_schema_origin]:
+                        class_name = featDict["fme_feature_type_original"]
+                    elif key_class_affix_origin in classmap and classmap[key_class_affix_origin]:
+                        class_name = featDict["fme_feature_type_sem_schema"]
+                    elif key_class_schema_origin in classmap and classmap[key_class_schema_origin]:
+                        class_name = featDict["fme_feature_type_sem_afixo"]
+                    else:
+                        class_name = featDict["fme_feature_type"]
+                    
+                    if classmap[key_class_origin] == class_name:
+                        if key_filter in classmap:
+                            if not self.evaluateFilter(featDict, classmap[key_filter]):
                                 continue
-                        
+
                         mappedFeat["fme_feature_type"] = classmap[key_class_destiny]
 
-                        if default_key in classmap:
-                            for default in classmap[default_key]:
+                        if key_class_affix_destiny in classmap and classmap[key_class_affix_destiny]:
+                            pass
+                        elif key_affix_destiny in self.mappingDict and 'tipo' in self.mappingDict[key_affix_destiny]:
+                            if self.mappingDict[key_affix_destiny]['tipo'] == 'sufixo':
+                                mappedFeat["fme_feature_type"] = mappedFeat["fme_feature_type"] + self.mappingDict[key_affix_destiny][featDict["$GEOM_TYPE"]]
+
+                            if self.mappingDict[key_affix_destiny]['tipo'] == 'prefixo':
+                                mappedFeat["fme_feature_type"] = self.mappingDict[key_affix_destiny][featDict["$GEOM_TYPE"]] + mappedFeat["fme_feature_type"]
+                        
+                        if key_class_schema_destiny in classmap and classmap[key_class_schema_destiny]:
+                            pass
+                        elif key_schema_destiny in self.mappingDict and self.mappingDict[key_schema_destiny]:
+                            mappedFeat["fme_feature_type"] = self.mappingDict[key_schema_destiny] + '.' + mappedFeat["fme_feature_type"]
+
+
+                        if key_default in classmap:
+                            for default in classmap[key_default]:
                                 mappedFeat[default["nome_atributo"]] = default["valor"]
                     
-                        if "traducao_atributos" in classmap:
-                            for attmap in classmap['traducao_atributos']:
+                        if "mapeamento_atributos" in classmap:
+                            for attmap in classmap['mapeamento_atributos']:
                                 if attmap[key_attr_origin] in featDict:
                                     if attmap[key_attr_destiny] not in mappedFeat:
                                         mappedFeat[attmap[key_attr_destiny]] = featDict[attmap[key_attr_origin]]
                                     if "traducao" in attmap:
                                         for valuemap in attmap["traducao"]:
                                             if u"{0}".format(valuemap[key_value_origin]) == u"{0}".format(featDict[attmap[key_attr_origin]]) and ("sentido" not in valuemap or ("sentido" in valuemap and valuemap["sentido"] == self.mappingType)):
-                                                mappedFeat[attmap[key_attr_destiny]] = valuemap[key_value_destiny]
+                                                if "concatenar" in valuemap and attmap[key_attr_destiny] in mappedFeat and mappedFeat[attmap[key_attr_destiny]]:
+                                                    mappedFeat[attmap[key_attr_destiny]] = mappedFeat[attmap[key_attr_destiny]] + ' | ' + valuemap[key_value_destiny]
+                                                else:
+                                                    mappedFeat[attmap[key_attr_destiny]] = valuemap[key_value_destiny]
                         
-                        if "mapeamento_atributos" in classmap:
-                            for attmap in classmap['mapeamento_atributos']:
+                        if "mapeamento_multiplo" in classmap:
+                            for attmap in classmap['mapeamento_multiplo']:
                                 if "sentido" not in attmap or ("sentido" in attmap and attmap["sentido"] == self.mappingType):
                                     if all([self.evaluateExpression(featDict, condition) for condition in attmap[key_group_origin]]):
                                         for valuemap in attmap[key_group_destiny]:
-                                            mappedFeat[valuemap["nome_atributo"]] = valuemap["valor"]
+                                            if "concatenar" in valuemap and valuemap["nome_atributo"] in mappedFeat and mappedFeat[valuemap["nome_atributo"]]:
+                                                mappedFeat[valuemap["nome_atributo"]] = mappedFeat[valuemap["nome_atributo"]] + ' | ' + valuemap["valor"]
+                                            else:
+                                                mappedFeat[valuemap["nome_atributo"]] = valuemap["valor"]
+                        
+                        if key_agregar in classmap:
+                            mappedFeat["AGGREGATE_GEOM"] = classmap[key_agregar]
+                        elif key_agregar in self.mappingDict:
+                            mappedFeat["AGGREGATE_GEOM"] = self.mappingDict[key_agregar]
+                        
                         break
                                     
         return mappedFeat
@@ -136,17 +217,24 @@ class FeatureProcessor(object):
                 feat.setAttribute(attr, u'{0}'.format(value))
             else:
                 feat.setAttribute(attr, None)
+        if "AGGREGATE_GEOM" in featDict and featDict["AGGREGATE_GEOM"]:
+            feat.buildAggregateFeat([feat])
         return feat
 
     def input(self,feature):
-        feat = self.buildFeatureDict(feature)
-        outputFeatDict = self.convertFeature(feat)
-        if "CLASS_NOT_FOUND" in outputFeatDict and not outputFeatDict["CLASS_NOT_FOUND"]:
-            feature.setAttribute("CLASS_NOT_FOUND", u'True')
-            self.pyoutput(feature)
+        if feature.numParts(True, False) > 1:
+            features = feature.splitAggregate(True)
         else:
-            outputFeature = self.mapDictToFeature(feature, outputFeatDict)
-            self.pyoutput(outputFeature)
+            features = [feature]
+        for simple_feature in features:
+            feat = self.buildFeatureDict(simple_feature)
+            outputFeatDict = self.convertFeature(feat)
+            if "CLASS_NOT_FOUND" in outputFeatDict and not outputFeatDict["CLASS_NOT_FOUND"]:
+                simple_feature.setAttribute("CLASS_NOT_FOUND", u'True')
+                self.pyoutput(simple_feature)
+            else:
+                outputFeature = self.mapDictToFeature(simple_feature, outputFeatDict)
+                self.pyoutput(outputFeature)
 
     def close(self):
         pass
