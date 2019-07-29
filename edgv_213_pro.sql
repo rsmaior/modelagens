afc0a30703ec2288be32c574beaf8621b3b0e4b9
Felipe Diniz <diniz.ime@gmail.com>
2019-07-29 15:50:51 -0300

    correção MF

--- edgv_213_pro.sql
+++ edgv_213_pro.sql
@@ -6,7 +6,7 @@ SET search_path TO pg_catalog,public,edgv,dominios;
 
 CREATE TABLE public.db_metadata(
 	 edgvversion varchar(50) NOT NULL DEFAULT 'EDGV 2.1.3 Pro',
-	 dbimplversion varchar(50) NOT NULL DEFAULT '5.3',
+	 dbimplversion varchar(50) NOT NULL DEFAULT '5.4',
 	 CONSTRAINT edgvversioncheck CHECK (edgvversion = 'EDGV 2.1.3 Pro')
 );
 INSERT INTO public.db_metadata (edgvversion, dbimplversion) VALUES ('EDGV 2.1.3 Pro','5.3');
