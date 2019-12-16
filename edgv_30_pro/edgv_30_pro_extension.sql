6221cc88993f02e852080b27b833e1b599dc62b6
Felipe Diniz <diniz.ime@gmail.com>
2019-12-16 05:46:39 -0300

    edgv 3 pro

---
+++ edgv_30_pro/edgv_30_pro_extension.sql
@@ -0,0 +1,154 @@
+--########################################################
+-- Função explodir multi geometrias
+CREATE EXTENSION IF NOT EXISTS hstore;
+
+CREATE OR REPLACE FUNCTION public.explode_geom()
+  RETURNS trigger AS
+$BODY$
+    DECLARE querytext1 text;
+    DECLARE querytext2 text;
+    DECLARE r record;
+    BEGIN
+
+	IF ST_NumGeometries(NEW.geom) > 1 THEN
+
+		querytext1 := 'INSERT INTO ' || quote_ident(TG_TABLE_SCHEMA) || '.' || quote_ident(TG_TABLE_NAME) || '(';
+		querytext2 := 'geom) SELECT ';
+
+		FOR r IN SELECT (each(hstore(NEW))).* 
+		LOOP
+			IF r.key <> 'geom' AND r.key <> 'id' THEN
+				querytext1 := querytext1 || quote_ident(r.key) || ',';
+				IF r.value <> '' THEN
+					querytext2 := querytext2 || quote_literal(r.value) || ',';
+				ELSE
+					querytext2 := querytext2 || 'NULL' || ',';					
+				END IF;
+			END IF;
+		END LOOP;
+
+		IF TG_OP = 'UPDATE' THEN
+			EXECUTE 'DELETE FROM ' || quote_ident(TG_TABLE_SCHEMA) || '.' || quote_ident(TG_TABLE_NAME) || ' WHERE id = ' || OLD.id;
+		END IF;
+
+
+		querytext1 := querytext1  || querytext2;
+		EXECUTE querytext1 || 'ST_Multi((ST_Dump(ST_AsEWKT(' || quote_literal(NEW.geom::text) || '))).geom);';
+		RETURN NULL;
+	ELSE
+		RETURN NEW;
+	END IF;
+    END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION public.explode_geom()
+  OWNER TO postgres;
+
+GRANT EXECUTE ON TABLE public.explode_geom TO PUBLIC;
+
+--########################################################
+-- Cria trigger de explodir multi geometrias
+
+DO $$DECLARE r record;
+BEGIN
+	FOR r in select f_table_schema, f_table_name from public.geometry_columns
+    LOOP
+	IF r.f_table_schema = 'edgv' THEN
+		EXECUTE 'CREATE TRIGGER a_explode_geom BEFORE INSERT OR UPDATE ON edgv.' || quote_ident(r.f_table_name) || ' FOR EACH ROW EXECUTE PROCEDURE public.explode_geom()';
+	END IF;
+    END LOOP;
+END$$;
+
+--########################################################
+--Cria tabela de estilos
+
+CREATE TABLE public.layer_styles
+(
+  id serial NOT NULL,
+  f_table_catalog character varying,
+  f_table_schema character varying,
+  f_table_name character varying,
+  f_geometry_column character varying,
+  stylename character varying(255),
+  styleqml text,
+  stylesld xml,
+  useasdefault boolean,
+  description text,
+  owner character varying(30),
+  ui xml,
+  update_time timestamp without time zone DEFAULT now(),
+  CONSTRAINT layer_styles_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.layer_styles OWNER TO postgres;
+
+GRANT ALL ON TABLE public.layer_styles TO public;
+
+--########################################################
+--Cria função que atualiza o nome do banco na tabela de estilos
+
+CREATE OR REPLACE FUNCTION public.estilo()
+  RETURNS integer AS
+$BODY$
+    UPDATE public.layer_styles
+        SET f_table_catalog = (select current_catalog);
+    SELECT 1;
+$BODY$
+  LANGUAGE sql VOLATILE
+  COST 100;
+ALTER FUNCTION public.estilo()
+  OWNER TO postgres;
+
+GRANT EXECUTE ON TABLE public.estilo TO PUBLIC;
+
+
+--########################################################
+--Cria tabela menu profile
+
+CREATE TABLE public.menu_profile
+(
+    id serial NOT NULL,
+    nome_do_perfil text NOT NULL,
+    descricao text,
+    perfil json NOT NULL,
+    ordem_menu json NOT NULL,
+    CONSTRAINT menu_profile_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.menu_profile
+    OWNER to postgres;
+
+GRANT ALL ON TABLE public.menu_profile TO PUBLIC;
+
+--########################################################
+--Cria tabela de regras
+CREATE TABLE public.layer_rules
+(
+    id serial NOT NULL,
+    camada text NOT NULL,
+    tipo_regra text NOT NULL,
+    nome text NOT NULL,
+    cor_rgb text NOT NULL,
+    regra text NOT NULL,
+    tipo_estilo text NOT NULL,
+    atributo text NOT NULL,
+    descricao text NOT NULL,
+    ordem integer NOT NULL,
+    CONSTRAINT layer_rules_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.layer_rules
+    OWNER to postgres;
+
+GRANT ALL ON TABLE public.layer_rules TO PUBLIC;
